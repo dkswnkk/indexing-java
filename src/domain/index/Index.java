35bb3ba6ea57657eb75a0d3b5fd7b36c927897d4
dkswnkk <jjoy9810@gmail.com>
2022-12-12 04:09:00 +0900

    feat: 인덱싱 기능 추가

---
+++ src/domain/index/Index.java
@@ -0,0 +1,63 @@
+package domain.index;
+
+import java.util.*;
+import java.io.*;
+
+public class Index {
+
+    private final String path;
+    private static final String FILE_NAME = "index.txt";
+
+    public Index(String path) {
+        this.path = path;
+    }
+
+    public void indexing(File file) throws IOException {
+        String[] indexingFiles = getIndexingFiles();
+        Map<String, Map<String, Integer>> record = new HashMap<>();
+        Map<String, Integer> dataCount = new HashMap<>();
+
+        for (String indexingFile : indexingFiles) {
+            BufferedReader data = new BufferedReader(new FileReader(path + '/' + indexingFile));
+            String content;
+            List<String> contents = new ArrayList<>();
+            while ((content = data.readLine()) != null) {
+                record.computeIfAbsent(content, k -> new HashMap<>());
+                contents.add(content);
+            }
+            for (String _content : contents) {
+                record.get(_content).put(indexingFile, record.get(_content).getOrDefault(indexingFile, 0) + 1);
+                dataCount.put(_content, dataCount.getOrDefault(_content, 0) + 1);
+            }
+        }
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+        for (String key : record.keySet()) {
+            LinkedList<Map.Entry<String, Integer>> entryList = new LinkedList<>(record.get(key).entrySet());
+            entryList.sort((o1, o2) -> o2.getValue().compareTo(o1.getValue()));
+
+            writer.write(key + ' ' + dataCount.get(key) + ' ');
+            for (Map.Entry<String, Integer> stringIntegerEntry : entryList) {
+                writer.write(stringIntegerEntry.getKey() + ' ' + stringIntegerEntry.getValue() + ' ');
+                writer.flush();
+            }
+            writer.newLine();
+        }
+    }
+
+    public File createFile() {
+        return new File(getParentDirectoryPath() + '/' + FILE_NAME);
+    }
+
+    private String[] getIndexingFiles() {
+        FilenameFilter filter = (file, name) -> name.contains(".txt");
+        return new File(path).list(filter);
+    }
+
+    private String getParentDirectoryPath() {
+        int index = path.lastIndexOf("/");
+        return path.substring(0, index);
+    }
+
+
+}
