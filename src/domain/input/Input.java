7e9234221da92ab6fc8540cce61ac2b787c40a05
dkswnkk <jjoy9810@gmail.com>
2022-12-14 03:41:13 +0900

    refactor: Input 기능 수정

--- src/domain/input/Input.java
+++ src/domain/input/Input.java
@@ -5,23 +5,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Input {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public String getPath() throws IOException {
+        System.out.println("[주의] .txt 문서의 데이터가 담긴 폴더를 입력하세요. ex) C:\\java_special\\input");
         System.out.print("폴더 입력: ");
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         return br.readLine();
     }
 
-    public Integer setCommand() throws Exception {
+    public String setCommand() throws Exception {
         System.out.print("1:인덱싱단계 2:검색어입력 3:종료 중 명령어를 입력하세요: ");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Integer commend = Integer.parseInt(br.readLine());
+        String commend = br.readLine();
         isValidCommand(commend);
-        return Integer.parseInt(br.readLine());
+        return commend;
     }
 
-    private void isValidCommand(Integer command) throws Exception {
-        if (command != 1 && command != 2 && command != 3) {
+    private void isValidCommand(String command) throws Exception {
+        if (!command.equals("1") && !command.equals("2") && !command.equals("3")) {
             throw new Exception("존재하지 않는 명령어입니다.");
         }
     }
