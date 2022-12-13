747a20024a19edff00071e4e6ca44be240d26eba
dkswnkk <jjoy9810@gmail.com>
2022-12-14 03:41:18 +0900

    feat: 메인 로직 추가

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
+import domain.index.Index;
+import domain.input.Input;
+import domain.search.Search;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import static domain.search.Search.getIndexTxt;
+import static domain.search.Search.isExistsIndexTxt;
+
 public class Main {
     public static void main(String[] args) throws Exception {
+        Input input = new Input();
+        String directory = input.getPath();
 
+        while (true) {
+            switch (input.setCommand()) {
+                case "1": {
+                    Index index = new Index(directory);
+                    index.indexing(index.createFile());
+                    break;
+                }
+                case "2": {
+                    if (!isExistsIndexTxt(getIndexTxt(directory))) {
+                        System.out.println("인덱싱 먼저 수행주세요!!!");
+                        break;
+                    }
+                    System.out.print("검색어를 입력하세요: ");
+                    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                    Search search = new Search(directory, br.readLine());
+                    search.searchQuery();
+                    search.loadFileData();
+                    search.print();
+                    break;
+                }
+                default: {
+                    System.exit(1);
+                    break;
+                }
+            }
+        }
     }
 }
